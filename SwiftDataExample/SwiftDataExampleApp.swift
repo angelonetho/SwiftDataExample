e061c5e50fae96b53705f37ded5c35c45212185e
Angelo Andrioli <angelonetho@gmail.com>
2025-10-03 09:35:01 -0300

    Initial Commit

---
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleApp.swift
+//  SwiftDataExample
+//
+//  Created by Angelo Andrioli Netho on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
